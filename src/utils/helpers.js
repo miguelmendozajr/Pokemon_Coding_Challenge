4e96808aa377675055e478b0736559a557cc2e9c
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-09-23 19:25:55 -0600

    feat: implement filtering logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,11 +3,15 @@ export function sortData(data, sort) {
 
   // While sort state isn't changed, return data in a-z format
   if (!sort) return data.sort((a, b) => a.name.localeCompare(b.name));
-  // else return data in z-a format
+  // Else, return data in z-a format
   return data.sort((a, b) => b.name.localeCompare(a.name));
 }
 
 export function filterData(data, filter) {
   // TODO: Implement the filtering logic
-  return data;
+
+  // If there's a filter, apply it and return filtered data
+  if (filter) return data.filter((item) => item.name.startsWith(filter));
+  // Else, return the unfiltered data
+  return data
 }
